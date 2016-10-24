5fa848eab22d183162d3b73d6e6e69de662b59d4
jonathandavidblack <jonnybl915@gmail.com>
2016-10-24 14:12:53 -0400

    deleted app-ok.js file

--- js/app.js
+++ js/app.js
@@ -3,3 +3,20 @@ var categoryListings = [
    {catName: "Nonfiction" ,   subcatList: ['Biography', 'Business', 'Education', 'Health', 'Philosophy', 'Self-Help'] },
    {catName: "Miscellaneous" ,   subcatList: ['Cooking','Crafts','Espanol', 'Medicine'] },
 ]
+
+var appContainer = document.querySelector("#app-container");
+
+var bookModel = Backbone.Model.extend({
+  url: "",
+  initialize: function(inputValue){
+    this.url = "https://www.googleapis.com/books/v1/volumes?q=subject:" + inputValue;
+  }
+})
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    "show-books/:bookName": 'showBooks',
+    '': "showHomePage"
+  },
+
+  
