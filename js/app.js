390f2e9b57140f7802265d746deb4f6d34c512e3
jonathandavidblack <jonnybl915@gmail.com>
2016-10-25 15:16:09 -0400

    home view has all info

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ var contentArea = document.querySelector(".content-area")
 
 var BookModel = Backbone.Model.extend({
     parse: function(parsedResponse) {
-    return parsedResponse.volumeInfo;
-      console.log("parsing volume Info: ", parsedResponse.volumeInfo);
+        return parsedResponse.volumeInfo;
+        console.log("parsing volume Info: ", parsedResponse.volumeInfo);
     },
 })
 
@@ -30,9 +30,9 @@ var BookCollection = Backbone.Collection.extend({
     model: BookModel,
 
     parse: function(serverResponse) {
-    console.log("parsing the response: ", serverResponse);
-    return serverResponse.items;
-  },
+        console.log("parsing the response: ", serverResponse);
+        return serverResponse.items;
+    },
     url: "",
     initialize: function(categoryName) {
         this.url = "https://www.googleapis.com/books/v1/volumes?q=subject:" + categoryName;
@@ -53,46 +53,53 @@ var AppRouter = Backbone.Router.extend({
         var bookCollectonInstance = new BookCollection(categoryName);
         bookCollectonInstance.fetch().then(function(serverRes) {
             console.log("Parsed COLLECTION INSTANCE: ", bookCollectonInstance);
-            contentArea.innerHTML += '<h2>' + categoryName.toUpperCase() + '</h2>';
-            bookCollectonInstance.models.forEach(function(bbModl, i){
 
-            var titles = bbModl.get("title");
-            var imageSrc = bbModl.get("imageLinks").smallThumbnail;
-              contentArea.innerHTML += '<div class="bookCard col-1-4">'
-              contentArea.innerHTML +=      '<img src="' + imageSrc + '"/>';
-              contentArea.innerHTML +=      '<p>' + titles + '</p>' + '</div>';
-              contentArea.innerHTML += ""
-            console.log("IMAGE SOURCES: ", imageSrc);
-            
+            bookCollectonInstance.models.forEach(function(bbModl, i) {
+                var builderString = '';
 
+                var titles = bbModl.get("title");
+                var imageSrc = bbModl.get("imageLinks").smallThumbnail;
+                console.log("TITLES: ", titles);
+                console.log("IMAGE SOURCES",imageSrc);
+                builderString += '<img alt="noIMG" src="' + imageSrc + '"/>';
+                builderString += '<p>' + titles + '</p>';
+                // console.log("BBMODEL: ", bbModl);
+                // builderString += '<img alt="noIMG" src="' + imageSrc + '"/>';
+                // builderString += '<p>' + titles + '</p>';
+                // builderString += "</div>"
+                // console.log("IMAGE SOURCES: ", imageSrc);
+                //
+                contentArea.innerHTML += builderString;
+            })
         })
-      })
     },
 
     showBook: function(bookName) {
-      var bookModelInstance = new BookModel(bookName);
-      bookModelInstance.fetch().then(function(){
-        console.log(bookModelInstance);
-      })
+        var bookModelInstance = new BookModel(bookName);
+        bookModelInstance.fetch().then(function() {
+            console.log(bookModelInstance);
+        })
     },
 
     HomePage: function() {
-
+        var builderString = "";
         for (var i = 0; i < categoryListings.length; i++) {
 
             var umbrellaCategory = categoryListings[i].catName;
-            console.log(categoryListings[i]);
-            contentArea.innerHTML += "<th> <a class='umbrella-category' href=''>" + umbrellaCategory + "</a> </th> ";
-            var subCategoryArray = categoryListings[i].subcatList;
-            console.log(subCategoryArray);
+            builderString += "<div class='category-list'>"
+            builderString += "<th> <a class='umbrella-category' href=''>" + umbrellaCategory + "</a> </th> ";
 
-            for (var i = 0; i < subCategoryArray.length; i++) {
-                var subCategory = subCategoryArray[i];
-                contentArea.innerHTML += "<ul>";
-                contentArea.innerHTML += "<li class='sub-category'> <a href=''>" + subCategory + "</a> </tr>";
-                contentArea.innerHTML += "</ul>";
+            var subCategoryArray = categoryListings[i].subcatList;
+            console.log("subCategoryArray: ", subCategoryArray.length);
+
+            for (var x = 0; x < subCategoryArray.length; x++) {
+                var subCategory = subCategoryArray[x];
+                builderString += "<li class='sub-category'> <a href=''>" + subCategory + "</a> </li>";
 
             }
+            builderString += "</div>"
+            contentArea.innerHTML = builderString;
+
         }
     }
 });
