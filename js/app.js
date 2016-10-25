42baf05db69a71db8d4d966189c1fb94f340a055
jonathandavidblack <jonnybl915@gmail.com>
2016-10-25 07:50:43 -0400

    added styling and put first ul on page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-var categoryListings = [
-   {catName: "Fiction" , subcatList: ['Drama','Literature','Mystery', 'Poetry','Romance'] },
-   {catName: "Nonfiction" ,   subcatList: ['Biography', 'Business', 'Education', 'Health', 'Philosophy', 'Self-Help'] },
-   {catName: "Miscellaneous" ,   subcatList: ['Cooking','Crafts','Espanol', 'Medicine'] },
-]
+var categoryListings = [{
+    catName: "Fiction",
+    subcatList: ['Drama', 'Literature', 'Mystery', 'Poetry', 'Romance']
+}, {
+    catName: "Nonfiction",
+    subcatList: ['Biography', 'Business', 'Education', 'Health', 'Philosophy', 'Self-Help']
+}, {
+    catName: "Miscellaneous",
+    subcatList: ['Cooking', 'Crafts', 'Espanol', 'Medicine']
+}, ]
+
 //Three routes:
 //ROUTE ===== FETCH ===== RENDER //
 //1. Home, 2.Books-main, 3. Books
@@ -13,43 +19,51 @@ var categoryListings = [
 var contentArea = document.querySelector(".content-area")
 
 var BookCollection = Backbone.Collection.extend({
-  url: "",
-  initialize: function(categoryName){
-    this.url = "https://www.googleapis.com/books/v1/volumes?q=subject:" + categoryName;
-  }
+    url: "",
+    initialize: function(categoryName) {
+        this.url = "https://www.googleapis.com/books/v1/volumes?q=subject:" + categoryName;
+    }
 })
 
 var AppRouter = Backbone.Router.extend({
-  routes: {
-    "books/:categoryName": 'showBooks',
-    '': "HomePage"
-  },
+    routes: {
+        "books/:categoryName": 'showBooks',
+        '': "HomePage"
+    },
 
-  showBooks: function(categoryName){
-    var bookCollectonInstance = new BookCollection(categoryName);
-    bookCollectonInstance.fetch().then(function(){
-      console.log(bookCollectonInstance);
-    })
-  },
+    showBooks: function(categoryName) {
+        var bookCollectonInstance = new BookCollection(categoryName);
+        bookCollectonInstance.fetch().then(function() {
+            console.log(bookCollectonInstance);
+        })
+    },
 
-  HomePage: function(){
-    console.log("hello there");
-    //perfom loops to insert row info dynamically;
-    for (var i = 0; i < categoryListings.length; i++) {
-      categoryListings[i]
+
+    HomePage: function() {
+        console.log("hello there");
+        //perfom loops to insert row info dynamically;
+        contentArea.innerHTML = '';
+        for (var i = 0; i < categoryListings.length; i++) {
+            var bigCategory = categoryListings[i].catName;
+            console.log(categoryListings[i]);
+            contentArea.innerHTML += "<th> <a class='umbrella-category' href=''>" + bigCategory + "</a> </th>";
+            console.log(bigCategory);
+            var subCategoryArray = categoryListings[i].subcatList;
+            for (var i = 0; i < subCategoryArray.length; i++) {
+                var subCategory = subCategoryArray[i];
+                console.log(subCategory);
+                contentArea.innerHTML += "<ul>";
+                contentArea.innerHTML += "<li class='sub-category'> <a href=''>" + subCategory + "</a> </tr>";
+            }
+            contentArea.innerHTML +="</ul>";
+        }
     }
-    contentArea.innerHTML += "<th> <a class='umbrella-category' href=''>" + "Fiction" + "</a> </th>";
-    contentArea.innerHTML += "<th> <a class='umbrella-category' href=''>" + "Mystery" + "</a> </th>"
-    contentArea.innerHTML += "<th> <a class='umbrella-category' href=''>" + "Nonfiction" + "</a> </th>"
-
-
-   }
 });
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
 
-  var appRouter = new AppRouter();
+    var appRouter = new AppRouter();
 
-  Backbone.history.start();
-  //keeps a log so as to go to previous hash on backspace
+    Backbone.history.start();
+    //keeps a log so as to go to previous hash on backspace
 })
